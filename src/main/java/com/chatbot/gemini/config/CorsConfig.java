856d0f636fe1c811f702f4246c51855e0eb2e172
AkhilYaduwanshi <akhilyaduwanshi41@gmail.com>
2026-08-09 16:43:47 +0530

    fix: allow production frontend cors

--- src/main/java/com/chatbot/gemini/config/CorsConfig.java
+++ src/main/java/com/chatbot/gemini/config/CorsConfig.java
@@ -9,9 +9,11 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://smart-email-generator-sigma.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
